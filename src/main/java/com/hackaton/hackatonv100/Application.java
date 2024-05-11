fdb6d2c553332558a8e3a06c98d470ce35133be4
IAmNotAnton <kias.22@uni-dubna.ru>
2024-05-11 18:42:34 +0300

    Окончательная версия для сдачи на хакатон

--- src/main/java/com/hackaton/hackatonv100/Application.java
+++ src/main/java/com/hackaton/hackatonv100/Application.java
@@ -1,13 +1,30 @@
 package com.hackaton.hackatonv100;
 
+import com.hackaton.hackatonv100.preview.Preview;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
+	public static boolean previewMode = false;
 
-	public static void main(String[] args) {
+	public static void main(String[] args)  {
+		for(var arg: args) {
+			System.out.println(arg);
+			if(arg.equals("-preview")) {
+				System.out.println("PREVIEW");
+				previewMode = true;
+			}
+		}
+
 		SpringApplication.run(Application.class, args);
+
 	}
+
 
 }
